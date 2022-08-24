7a9ede39ee0c5bb0b2d7f4c8380dff8c037ba063
Monique <monique.meas@gmail.com>
2022-08-24 13:19:56 -0400

    add server.js

---
+++ server.js
@@ -0,0 +1,20 @@
+const { application } = require('express');
+const express = require('express');
+const PORT = process.env.Port || 3001;
+const app = express();
+
+//Express middleware
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+
+ //Default response for any other request (Not found)
+ app.use((req, res) => {
+    res.status(404).end();
+ });
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
